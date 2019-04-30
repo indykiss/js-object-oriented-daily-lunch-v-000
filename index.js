555f2b32762a020847f12229e9c5b731c1262d5b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-30 17:48:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodID = 0
+
+class Neighborhood() {
+  constructor(name) {
+    this.id = ++neighborhoodID;
+    this.name = name;
+    store.neighborhoods.push(this);
+  }
+}
